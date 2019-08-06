817058cb79f896a00b6cef272149f88e843fd2c8
elliottcoe <elliott@ontapmedia.co.uk>
2019-08-06 20:17:28 +0100

    setup

---
+++ src/Layouts/DefaultLayout.php
@@ -0,0 +1,39 @@
+<?php
+namespace ProtectedNet\FrontendTest\Layouts;
+
+use Packaged\Dispatch\ResourceManager;
+use ProtectedNet\FrontendTest\Components\Button\Button;
+use ProtectedNet\FrontendTest\Components\Link\Link;
+use ProtectedNet\FrontendTest\Components\Nav\Nav;
+use ProtectedNet\FrontendTestFramework\Layouts\DefaultLayout\DefaultLayout as GlobalDefaultLayout;
+
+class DefaultLayout extends GlobalDefaultLayout
+{
+  /** TestDefaultLayout constructor.*/
+  public function __construct()
+  {
+    ResourceManager::resources()->requireCss('styles/global.min.css', null, 20);
+    ResourceManager::resources()->requireJs('js/libraries.min.js', null, 20);
+  }
+
+  protected function _getNav()
+  {
+    if($this->_nav === false)
+    {
+      $this->_nav = Nav::i()->setNavItems(
+        [
+          Link::i('Home'),
+          Link::i('Jobs'),
+          Link::i('Candidates'),
+          Link::i('About'),
+          Link::i('Blog'),
+          Link::i('Contacts'),
+          Button::i('Sign In')->ghost()
+        ]
+      );
+    }
+
+    return $this->_nav;
+  }
+
+}
